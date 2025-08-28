9022a0b64115b12699c981f541a00b9faa68d6d3
Olaf Gaza <c-ogaza@xpansiv.com>
2025-08-28 11:28:35 +0200

    digging_into: improve command creation from CLI so they inlude their params

--- DiggingInto/src/cli.js
+++ DiggingInto/src/cli.js
@@ -2,7 +2,30 @@ import minimist from "minimist";
 
 export function getCommandFromCLI() {
   var args = minimist(process.argv.slice(2), {});
-  const [commandName] = args["_"];
 
-  return { name: !args.help ? commandName : "help" };
+  // printForDebugging(args);
+
+  const {
+    _: [name],
+    ...params
+  } = args;
+
+
+  const command = {
+    name: !args.help ? name : "help",
+    params,
+  };
+
+  return command;
+}
+
+function printForDebugging(args) {
+  console.log("args: ", args);
+
+  const {
+    _: [name],
+    ...params
+  } = args;
+
+  console.log("command: ", { name, params });
 }
