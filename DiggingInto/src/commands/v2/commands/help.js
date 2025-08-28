d0a3142516d27cafb68493ff7ac8488bdb4e8b7f
Olaf Gaza <c-ogaza@xpansiv.com>
2025-08-28 12:29:59 +0200

    digging_into: process_file command new case - process file loaded to stdin stream

--- DiggingInto/src/commands/v2/commands/help.js
+++ DiggingInto/src/commands/v2/commands/help.js
@@ -7,8 +7,10 @@ function handleHelpCommand() {
   console.log("--------------------------------------------------");
   console.log("digging_into CLI App");
   console.log("--------------------------------------------------");
-  console.log("digging_into test        runs test command");
-  console.log("digging_into --help      prints help");
+  console.log("digging_into process_file --path=\"test.txt\"   processes test.txt file");
+  console.log("cat test.txt | digging_into in         as abowe but using stdin");
+  console.log("digging_into test                      runs test command");
+  console.log("digging_into --help                    prints help");
   console.log("--------------------------------------------------");
   console.log("");
 }
--- DiggingInto/src/commands/v2/commands/processFile.js
+++ DiggingInto/src/commands/v2/commands/processFile.js
@@ -1,33 +1,62 @@
 import { readFile } from "node:fs";
+import getStdin from "get-stdin";
 import { createHandler } from "./createHandler.js";
 
 function canHandle({ name, params: { path } }) {
-  return name == "process_file" && !!path;
+  return (name == "process_file" && !!path) || name == "in";
 }
-function handle({ params: { path } }) {
-  processFile(path);
+function handle({ name, params: { path } }) {
+  switch (name) {
+    case "process_file":
+      processFile(path);
+      break;
+    case "in":
+      console.log("processing from stdin");
+      loadFromStdin();
+      break;
+
+    default:
+      break;
+  }
 }
 
 export const processFileHandler = createHandler(canHandle, handle);
 
-// file loading and processing
+// processing from stdin
+
+function loadFromStdin() {
+  getStdin().then(onLoadedFromStdin).catch(onStdinError);
+}
+
+function onLoadedFromStdin(contents) {
+  const processed = processString(contents.toString());
+  process.stdout.write(processed);
+}
+
+function onStdinError(err) {
+  console.log(err);
+}
+
+// processing from file
 
 function processFile(filepath) {
-  readFile(filepath, function onContentsLoaded(error, contents) {
-    if (error) {
-      console.error(error.toString());
-
-      return;
-    }
-
-    // write takes a buffer as its param
-    // so no need to stringify
-    // process.stdout.write(contents);
-
-    const processedContent = processString(contents.toString());
-    process.stdout.write(processedContent);
-  });
+  readFile(filepath, onFileContentsLoaded);
 }
+
+function onFileContentsLoaded(error, contents) {
+  if (error) {
+    console.error(error.toString());
+
+    return;
+  }
+  // write takes a buffer as its param
+  // so no need to stringify
+  // process.stdout.write(contents);
+  const processed = processString(contents.toString());
+  process.stdout.write(processed);
+}
+
+// input processing
 
 function processString(str) {
   return str.toUpperCase();
