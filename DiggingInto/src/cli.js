dfc6cac5ad4aa5647ada65e262e5e660d368f2a9
Olaf Gaza <c-ogaza@xpansiv.com>
2025-08-28 14:05:19 +0200

    digging_into: simple code taking in env variable from the CLI command

--- DiggingInto/src/cli.js
+++ DiggingInto/src/cli.js
@@ -1,15 +1,24 @@
 import minimist from "minimist";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export function getCommandFromCLI() {
   var args = minimist(process.argv.slice(2), {});
 
   // printForDebugging(args);
 
+  const basePath = process.env.BASE_PATH;
+  if (basePath) {
+    console.log("BASE_DIR ", path.join(__dirname, basePath));
+    // console.log("BASE_DIR ", path.join(__dirname, "\\..", basePath));
+  }
+
   const {
     _: [name],
     ...params
   } = args;
-
 
   const command = {
     name: !args.help ? name : "help",
--- DiggingInto/src/commands/v2/commands/help.js
+++ DiggingInto/src/commands/v2/commands/help.js
@@ -8,9 +8,10 @@ function handleHelpCommand() {
   console.log("digging_into CLI App");
   console.log("--------------------------------------------------");
   console.log("digging_into process_file --path=\"test.txt\"   processes test.txt file");
-  console.log("cat test.txt | digging_into in         as abowe but using stdin");
-  console.log("digging_into test                      runs test command");
-  console.log("digging_into --help                    prints help");
+  console.log("cat test.txt | digging_into in        as abowe but using stdin");
+  console.log("BASE_PATH=files digging_into ...      sets env variable BASE_PATH");
+  console.log("digging_into test                     runs test command");
+  console.log("digging_into --help                   prints help");
   console.log("--------------------------------------------------");
   console.log("");
 }
