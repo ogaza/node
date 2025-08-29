276e1a87a6afebd1745c3a4309d244bac9d2a08c
Olaf Gaza <c-ogaza@xpansiv.com>
2025-08-29 10:17:09 +0200

    digging_into: add content transformation to the file processing using streams

--- DiggingInto/src/commands/v2/commands/streamFile.js
+++ DiggingInto/src/commands/v2/commands/streamFile.js
@@ -3,10 +3,7 @@ import { createHandler } from "./createHandler.js";
 import { Transform } from "node:stream";
 
 function canHandle({ name, params: { path } }) {
-  return (
-    name == "stream_stdin" ||
-    name == "stream_file"
-  );
+  return name == "stream_stdin" || name == "stream_file";
 }
 function handle({ name, params: { path } }) {
   switch (name) {
@@ -26,14 +23,38 @@ export const streamFileHandler = createHandler(canHandle, handle);
 
 // processing file as it is being read from stdin
 function streamStdin() {
-  var stream = process.stdin;
-  stream.pipe(process.stdout);
+  var transformStream = new Transform({ transform });
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 }
 
 // processing from file
 
 function streamFile(filepath) {
-  const fileStream = createReadStream(filepath);
+  // short version:
+  createReadStream(filepath) // creates readstream
+    .pipe(new Transform({ transform })) // pipes it to writable transform stream
+    .pipe(process.stdout); // pipes resulting readable stream to the final writable stdout strean
 
-  fileStream.pipe(process.stdout);
+  // verbose version:
+  // var transformStream = new Transform({ transform });
+  // var fileStream = createReadStream(filepath);
+  // fileStream.pipe(transformStream).pipe(process.stdout);
+}
+
+function transform(chunk, _, next) {
+  // short node-v20 version:
+  next(null, String(chunk).toLocaleUpperCase());
+
+  // longer version:
+  // this.push(String(chunk).toLocaleUpperCase());
+  // next();
+
+  // to see that the process actually deals with chunks:
+  // note: each chunk takes about 64kB, so the file
+  // needs to be bigger than that if there are to be
+  // at least two iterations
+  // setTimeout(function () {
+  //   return next(null, String(chunk).toLocaleUpperCase());
+  // }, 1000);
+
 }
