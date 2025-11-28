4002f8cd964cd9ceda6373627e5e0e9cceab3b2a
Olaf Gaza <c-ogaza@xpansiv.com>
2025-11-28 11:08:30 +0100

    api in node: get recipes api with the data taken from the db using node-postgres

--- APIinNode/src/server.js
+++ APIinNode/src/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import path from "path";
-// const path = require("path");
+import db from "./db.js";
 
 const app = express();
 const port = 5000;
+
 app.use(express.static("static"));
-/**
- * app.[method]([route], [route handler])
- */
+
 app.get("/", handleRootGet);
+app.get("/recipes", handleGetRecipes);
 
 export function appStart() {
   // creates and starts a server for our API on a defined port
@@ -20,4 +20,14 @@ export function appStart() {
 function handleRootGet(req, res) {
   // sending back an HTML file that a browser can render on the screen.
   res.sendFile(path.resolve("pages/index.html"));
+}
+
+async function handleGetRecipes(req, res) {
+
+  const { rows } = await db.query(`SELECT * FROM recipes`);
+
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Cache-Control", "max-age: 0, no-cache");
+  res.writeHead(200);
+  res.end(JSON.stringify(rows));
 }
