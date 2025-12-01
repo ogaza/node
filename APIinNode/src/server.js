de11d1a2d7336cce5c5a08f5484371fd6b8fc9e3
Olaf Gaza <c-ogaza@xpansiv.com>
2025-12-01 18:10:09 +0100

    api in node: search recipe api enpoint

--- APIinNode/src/server.js
+++ APIinNode/src/server.js
@@ -10,6 +10,7 @@ app.use(express.static("static"));
 app.get("/", handleRootGet);
 app.get("/recipes", handleGetRecipes);
 app.get("/recipes/:id", handleGetRecipe);
+app.get("/search", handleSearch);
 
 export function appStart() {
   // creates and starts a server for our API on a defined port
@@ -37,10 +38,41 @@ async function handleGetRecipe(req, res) {
     params: { id },
   } = req;
 
-  const { rows } = await db.query(`SELECT * FROM recipes WHERE recipe_id = $1`, [id]);
+  const { rows } = await db.query(
+    `SELECT * FROM recipes WHERE recipe_id = $1`,
+    [id]
+  );
 
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Cache-Control", "max-age: 0, no-cache");
   res.writeHead(200);
   res.end(JSON.stringify(rows));
+}
+
+async function handleSearch({ query }, res) {
+  let { term, page } = query;
+
+  term = !!term ? term : "";
+  page = !!Number(page) ? Math.floor(Number(page)) : 1;
+
+  const pageLength = 5;
+  const offset = (page - 1) * pageLength;
+  const params = [`%${term}%`, offset, pageLength];
+
+  console.log(`getting page: ${page}`);
+  console.log("params", params);
+
+  const { rows } = await db.query(
+    `SELECT
+       *,
+       count(*) OVER()::INT AS total_count
+     FROM recipes
+     WHERE
+       title ILIKE $1
+     OFFSET $2 LIMIT $3`,
+    params
+  );
+
+  res.status(500).json({ rows });
+  // res.status(500).json({ message: "not implemented", page });
 }
