faa8901b6a0479301079c0d18c1522ddd6f0f1d8
Olaf Gaża <olaf.gaza@outlook.com>
2026-06-15 17:06:31 +0200

    api in node: commnents in the auth.js file

--- APIinNode/src/auth/auth.js
+++ APIinNode/src/auth/auth.js
@@ -1,5 +1,11 @@
 import * as bcrypt from "bcrypt";
 
+// The following two function were to be used during
+// an user creation for saving passowrds in the db
+// as a hash representation.
+//
+// This functionality does not exist yet, though.
+
 export const comparePasswords = (password, hash) => {
   return bcrypt.compare(password, hash);
 };
